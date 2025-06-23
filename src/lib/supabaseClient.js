badb7ea1d3b1c16a98129eeb7fdfc4966bb54b05
sicsee <nicolasdsgodinho@gmail.com>
2025-06-23 00:06:18 -0300

    2.0

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://zdrkzxijspubpkxpnadk.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkcmt6eGlqc3B1YnBreHBuYWRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MzU2MjYsImV4cCI6MjA2MTExMTYyNn0.KqxBOoCxGYsciAI-dnt0Lm_yl7FCfeBJ83lK5dgHZp4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
